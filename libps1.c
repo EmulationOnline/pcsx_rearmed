077e3321960b09f3ba9965d011eba7850e2aac06
Alex <alex_carbon@emulationonline.com>
2025-12-29 17:54:50 -0600

    resolve main link failures.

--- libps1.c
+++ libps1.c
@@ -1,17 +1,17 @@
 #include "corelib.h"
-#include "../libpcsxcore/misc.h"
-#include "../libpcsxcore/psxcounters.h"
-#include "../libpcsxcore/psxmem_map.h"
-#include "../libpcsxcore/new_dynarec/new_dynarec.h"
-#include "../libpcsxcore/cdrom.h"
-#include "../libpcsxcore/cdrom-async.h"
-#include "../libpcsxcore/cdriso.h"
-#include "../libpcsxcore/cheat.h"
-#include "../libpcsxcore/r3000a.h"
-#include "../libpcsxcore/gpu.h"
-#include "../libpcsxcore/database.h"
-#include "../plugins/dfsound/out.h"
-#include "../plugins/dfsound/spu_config.h"
+#include "libpcsxcore/misc.h"
+#include "libpcsxcore/psxcounters.h"
+#include "libpcsxcore/psxmem_map.h"
+#include "libpcsxcore/new_dynarec/new_dynarec.h"
+#include "libpcsxcore/cdrom.h"
+#include "libpcsxcore/cdrom-async.h"
+#include "libpcsxcore/cdriso.h"
+#include "libpcsxcore/cheat.h"
+#include "libpcsxcore/r3000a.h"
+#include "libpcsxcore/gpu.h"
+#include "libpcsxcore/database.h"
+#include "plugins/dfsound/out.h"
+#include "plugins/dfsound/spu_config.h"
 // #include "cspace.h"
 #include "frontend/main.h"
 // #include "menu.h"
@@ -21,8 +21,59 @@
 // #include "revision.h"
 
 
+#include <stdarg.h>
+#include <stdio.h>
+
+#include "frontend/plugin_lib.h"
+
 R3000Acpu *psxCore = NULL;
 uint32_t fbuffer_[VIDEO_WIDTH * VIDEO_HEIGHT];
+
+void SysPrintf(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stdout, fmt, args);
+    va_end(args);
+}
+
+// Input globals expected by some parts of the core/plugins
+int in_type[8];
+uint16_t in_keystate[8];
+int in_analog_left[8][2];
+int in_analog_right[8][2];
+int in_mouse[8][2];
+int multitap1;
+int multitap2;
+
+// Other expected globals
+struct rearmed_cbs pl_rearmed_cbs;
+
+#include "plugins/dfsound/out.h"
+
+static void (*emu_puts_cb)(const char *) = NULL;
+void corelib_set_puts(void (*cb)(const char *)) {
+    emu_puts_cb = cb;
+}
+
+// Audio driver stub for dfsound
+static int dummy_init(void) { return 0; }
+static void dummy_finish(void) {}
+static int dummy_busy(void) { return 0; }
+static void dummy_feed(void *data, int bytes) {}
+
+void out_register_libretro(struct out_driver *drv) {
+    drv->name = "libretro_dummy";
+    drv->init = dummy_init;
+    drv->finish = dummy_finish;
+    drv->busy = dummy_busy;
+    drv->feed = dummy_feed;
+}
+
+// Platform/Frontend stubs
+void plat_trigger_vibrate(int port, int low, int high) {}
+void pl_frame_limit(void) {}
+void pl_timing_prepare(int is_pal) {}
+void pl_gun_byte2(int port, unsigned char byte) {}
 
 EXPOSE
 void set_key(size_t key, char val) {
